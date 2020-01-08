f5937f576ac24460adaa57e26d1f11655ea71434
PreranaPandit <preranapandit07@gmail.com>
2020-01-08 12:42:02 +0545

    Search Map added::new map activity

--- app/src/main/java/com/example/prerana/googlemap/models/LatitudeLongitude.java
+++ app/src/main/java/com/example/prerana/googlemap/models/LatitudeLongitude.java
@@ -6,10 +6,35 @@ public class LatitudeLongitude {
     private double lon;
     private String marker;
 
+    private double v;
+    private double v2;
+
     public LatitudeLongitude(double lat, double lon, String marker) {
         this.lat = lat;
         this.lon = lon;
         this.marker = marker;
+    }
+
+    public LatitudeLongitude(double v, double v1) {
+
+        this.v = v;
+        this.v2 = v2;
+    }
+
+    public double getV() {
+        return v;
+    }
+
+    public void setV(double v) {
+        this.v = v;
+    }
+
+    public double getV2() {
+        return v2;
+    }
+
+    public void setV2(double v2) {
+        this.v2 = v2;
     }
 
     public double getLat() {
